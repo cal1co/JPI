3aba55f19e99272831d5b717e2869e2e2f7cb961
Alex King <calicoalix@gmail.com>
2022-08-31 21:15:39 +1000

    rough edit distance logic mapped

--- algo/algo.go
+++ algo/algo.go
@@ -10,6 +10,7 @@ type trieNode struct {
 	children [charSize]*trieNode
 	isEnd    bool
 	pos      int
+	length   int
 }
 
 type trie struct {
@@ -33,6 +34,7 @@ func (t *trie) insert(word string, index int) {
 	}
 	curr.isEnd = true
 	curr.pos = index
+	curr.length = len(word)
 }
 
 func (t *trie) find(word string, edits int) bool {
@@ -50,28 +52,31 @@ func (t *trie) find(word string, edits int) bool {
 	return curr.isEnd
 }
 
-// bfs := func(word string, node *trieNode, edits int, index int) []string {
-// 	if index == len(word) || edits < 0 {
-// 		return
-// 	}
-// 	idx := word[i] - 'a'
-// 	if node.children[idx] == nil {
-// 		edits -= 1
-// 		// run the bfs here and exclude the real child
-// 		nextNum := word[i + 1] - 'a'
-// 		for i := 0; i < len(node.children); i++ {
-// 			if node.children[i] != nil && node.children[i] != node.children[nextNum] {
-// 				bfs(word, node.children[i], edits, index + 1)
-// 			}
-// 		}
-// 	}
+bfs := func(word string, node *trieNode, edits int, index int, changes int) []string {
+	if index == len(word) || edits < 0 {
+		return
+	}
+	idx := word[i] - 'a'
+	if node.children[idx] == nil {
+		edits -= 1
+		// run the bfs here and exclude the real child
+		// nextNum := word[i + 1] - 'a'
+		for i := 0; i < len(node.children); i++ {
+			if node.children[i] != nil {
+				bfs(word, node.children[i], edits, index + 1, changes + 1)
+			}
+		}
+	}
 
-// 	bfs(word, node.children[idx], edits, index + 1)
+	bfs(word, node.children[idx], edits, index + 1, changes)
+	// a - runs 69, p - '', p - '', e - runs 58, l - runs 58
+	// appel will return the index of apple as well as a score of 0.6
+	// the score is calculated by dividing 3/5 (len(term) - edits made)/len(res)
 
-// 	if node.isEnd {
-// 		return node.pos
-// 	}
-// }(word, curr, edits, index)
+	if node.isEnd {
+		return map{node.pos : (node.length - changes) / len(word) }
+	}
+}(word, curr, edits, index)
 
 func Blah() []string {
 	arr := []string{}
