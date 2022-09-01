285f415089547318e22767bb30481e357ddad46a
Alex King <calicoalix@gmail.com>
2022-09-01 13:42:10 +1000

    fuzzy trie bfs with edits works

--- algo/algo.go
+++ algo/algo.go
@@ -1,6 +1,9 @@
 package algo
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 const (
 	charSize = 26
@@ -37,62 +40,77 @@ func (t *trie) insert(word string, index int) {
 	curr.length = len(word)
 }
 
-func (t *trie) find(word string, edits int) bool {
+// func (t *trie) find(word string, edits int) bool {
+// 	curr := t.root
+// 	for i := 0; i < len(word); i++ {
+// 		idx := word[i] - 'a'
+// 		if curr.children[idx] == nil {
+// 			return false
+// 		}
+// 		curr = curr.children[idx]
+// 	}
+// 	fmt.Println(curr.pos)
+
+// 	return curr.isEnd
+// }
+
+func (t *trie) find(word string, node *trieNode, edits int, index int, changes int) []map[int]float64 {
 	curr := t.root
-	for i := 0; i < len(word); i++ {
-		idx := word[i] - 'a'
-		if curr.children[idx] == nil {
-			return false
-		}
-		curr = curr.children[idx]
-	}
-	fmt.Println(curr.pos)
-
 	// run a bfs
-	return curr.isEnd
-}
-
-bfs := func(word string, node *trieNode, edits int, index int, changes int) []string {
-	if index == len(word) || edits < 0 {
-		return
-	}
-	idx := word[i] - 'a'
-	if node.children[idx] == nil {
-		edits -= 1
-		// run the bfs here and exclude the real child
-		// nextNum := word[i + 1] - 'a'
-		for i := 0; i < len(node.children); i++ {
-			if node.children[i] != nil {
-				bfs(word, node.children[i], edits, index + 1, changes + 1)
+	var slice []map[int]float64
+	var bfs func(word string, node *trieNode, edits int, index int, changes int) func(map[int]int)
+	bfs = func(word string, node *trieNode, edits int, index int, changes int) func(map[int]int) {
+		// fmt.Println("BFS CALLED", word, index)
+		if node.isEnd {
+			score := float64((node.length - changes)) / float64(len(word))
+			if score >= 0.6 {
+				slice = append(slice, map[int]float64{node.pos: score})
 			}
 		}
+		if index == len(word) || edits < 0 {
+			return nil
+		}
+		idx := word[index] - 'a'
+		if node.children[idx] == nil {
+			edits -= 1
+			// run the bfs here and exclude the real child
+			// nextNum := word[i + 1] - 'a'
+			for i := 0; i < len(node.children); i++ {
+				if node.children[i] != nil {
+					// fmt.Println(node.children[i])
+					bfs(word, node.children[i], edits, index+1, changes+1)
+				}
+			}
+		} else {
+			bfs(word, node.children[idx], edits, index+1, changes)
+		}
+
+		// // a - runs 69, p - '', p - '', e - runs 58, l - runs 58
+		// // appel will return the index of apple as well as a score of 0.6
+		// // the score is calculated by dividing 3/5 (len(term) - edits made)/len(res)
+		return nil
 	}
+	bfs(word, curr, edits, 0, 0)
+	fmt.Println(word, slice)
+	return slice
+}
 
-	bfs(word, node.children[idx], edits, index + 1, changes)
-	// a - runs 69, p - '', p - '', e - runs 58, l - runs 58
-	// appel will return the index of apple as well as a score of 0.6
-	// the score is calculated by dividing 3/5 (len(term) - edits made)/len(res)
-
-	if node.isEnd {
-		return map{node.pos : (node.length - changes) / len(word) }
-	}
-}(word, curr, edits, index)
-
-func Blah() []string {
-	arr := []string{}
+func Blah() [][]map[int]float64 {
+	start := time.Now()
+	var arr [][]map[int]float64
 	trie := initTrie()
 	words := []string{"ape", "app", "apple", "add", "aud"}
 	for i := 0; i < len(words); i++ {
 		trie.insert(words[i], i)
 	}
 	// match := []string{"ape", "app", "aid", "aud", "aplle"}
-	match := []string{"app", "ape", "apple", "add", "aplle", "aud"}
-	// match := []string{"app", "ape", "aid", "apple", "add"}
+	match := []string{"app", "ape", "apple", "add", "aplle", "aud", "aid"}
 	for i := 0; i < len(words); i++ {
-		matched := trie.find(match[i], 2)
-		if matched {
-			arr = append(arr, match[i])
+		matched := trie.find(match[i], trie.root, 1, 0, 0)
+		if len(matched) > 0 {
+			arr = append(arr, matched)
 		}
 	}
+	fmt.Println(time.Since(start))
 	return arr
 }
